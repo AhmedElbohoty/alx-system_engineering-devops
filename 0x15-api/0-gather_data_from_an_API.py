df62768f30c0effa61d3ee8a0e36d3678707e698
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-04-22 20:21:23 +0200

    Task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-For a given employee ID, returns information about his TODO list progress.
+For a given employee ID, returns information about his todo list progress.
 '''
 import requests
 import sys
@@ -8,7 +8,7 @@ import sys
 
 def get_employee_todo(employee_id):
     '''
-    For a given employee ID, returns information about his TODO list progress.
+    For a given employee ID, returns information about his todo list progress.
 
     Args:
         employee_id (str): the employee id.
@@ -20,28 +20,23 @@ def get_employee_todo(employee_id):
 
     timeout = 5
 
-    try:
-        # Get Employee info
-        employee_resp = requests.get(employee_url, timeout=timeout)
-        employee_name = employee_resp.json()['name']
+    # Get Employee info
+    employee_resp = requests.get(employee_url, timeout=timeout)
+    employee_name = employee_resp.json()['name']
 
-        # Get fetch todo list
-        todo_resp = requests.get(todo_url, timeout=timeout)
-        todos = todo_resp.json()
+    # Get fetch todo list
+    todo_resp = requests.get(todo_url, timeout=timeout)
+    todos = todo_resp.json()
 
-        total_tasks = len(todos)
-        done_tasks = sum(1 for task in todos if task['completed'])
+    total_tasks = len(todos)
+    done_tasks = sum(1 for task in todos if task['completed'])
 
-        print(f"Employee {employee_name} is done with tasks({
-              done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({
+        done_tasks}/{total_tasks}):")
 
-        for task in todos:
-            if task['completed']:
-                print(f"\t{task['title']}")
-
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
-        sys.exit(1)
+    for task in todos:
+        if task['completed']:
+            print(f"\t{task['title']}")
 
 
 if __name__ == "__main__":
