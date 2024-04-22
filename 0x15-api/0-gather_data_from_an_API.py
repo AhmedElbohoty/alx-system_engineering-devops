dd7e30d7e9a294c20009966c05780cfb253351a7
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-04-22 21:31:18 +0200

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -29,8 +29,8 @@ if __name__ == "__main__":
     total_tasks = len(todos)
     done_todos = sum(1 for task in todos if task.get("completed"))
 
-    print(f"Employee {employee_name} is done with tasks({\
-          done_todos}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks("
+          f"{done_todos}/{total_tasks}):")
 
     for task in todos:
         if task.get("completed"):
