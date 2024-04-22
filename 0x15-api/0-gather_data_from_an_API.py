6fa1c83fb1a2cc8699983530bc69e43a868a126a
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-04-22 20:50:40 +0200

    Task 2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,36 +9,29 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
 
-    def get_employee_todo(employee_id):
-        """
-        For a given employee ID, returns information about his todo list progress.
+    employee_id = sys.argv[1]
 
-        Args:
-            employee_id (str): the employee id.
-        """
-        base_url = "https://jsonplaceholder.typicode.com"
+    base_url = "https://jsonplaceholder.typicode.com"
 
-        employee_url = f"{base_url}/users/{employee_id}"
-        todo_url = f"{base_url}/todos?userId={employee_id}"
+    employee_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/todos?userId={employee_id}"
 
-        timeout = 5
+    timeout = 5
 
-        # Get Employee info
-        employee_resp = requests.get(employee_url, timeout=timeout)
-        employee_name = employee_resp.json().get("name")
+    # Get Employee info
+    employee_resp = requests.get(employee_url, timeout=timeout)
+    employee_name = employee_resp.json().get("name")
 
-        # Get fetch todo list
-        todo_resp = requests.get(todo_url, timeout=timeout)
-        todos = todo_resp.json()
+    # Get fetch todo list
+    todo_resp = requests.get(todo_url, timeout=timeout)
+    todos = todo_resp.json()
 
-        total_tasks = len(todos)
-        done_todos = sum(1 for task in todos if task.get("completed"))
+    total_tasks = len(todos)
+    done_todos = sum(1 for task in todos if task.get("completed"))
 
-        print(f"Employee {employee_name} is done with tasks({
-            done_todos}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks({
+        done_todos}/{total_tasks}):")
 
-        for task in todos:
-            if task.get("completed"):
-                print(f"\t{task["title"]}")
-
-    get_employee_todo(sys.argv[1])
+    for task in todos:
+        if task.get("completed"):
+            print(f"\t{task["title"]}")
