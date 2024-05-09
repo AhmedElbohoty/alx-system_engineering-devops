2f4a02469ef585ab57784ade8f133aef02b74966
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-05-09 03:26:03 +0300

    Fix issue

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -8,14 +8,6 @@ service { 'apache2':
   require => Package['apache2'],
 }
 
-file { '/var/www/html/wp-content/uploads':
-  ensure  => directory,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
-  recurse => true,
-}
-
 exec { 'fix':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
