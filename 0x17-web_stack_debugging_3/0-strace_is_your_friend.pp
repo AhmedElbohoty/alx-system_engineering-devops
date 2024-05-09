fc5048e29d51de3d05dfd93f5453b65f32bca649
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-05-09 03:32:05 +0300

    Fix issue

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,6 @@
-package { 'apache2':
-  ensure => installed,
-}
 
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Package['apache2'],
-}
 
 exec { 'fix':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin'
 }
