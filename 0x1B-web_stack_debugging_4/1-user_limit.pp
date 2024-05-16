656c0c3e92fd445b34b60e5b649577bffe3c58b1
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-05-16 20:20:13 +0300

    Task 2

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,16 @@
 # Change the OS configuration so that it is possible to login with the
 # holberton user and open a file without any error message.
 
+$user = 'holberton'
+$limi = '50000'
 $command_path = '/usr/local/bin/:/bin/'
 
-exec { 'increase-hard-limit':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => $command_path
+exec { "increase-hard-file-limit-for-${user}-user":
+  command => "sed -i \"/${user} hard/s/5/${limi}/\" /etc/security/limits.conf",
+  path    => $command_path,
 }
 
-exec { 'increase-soft-limit':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => $command_path
+exec { "increase-soft-file-limit-for-${user}-user":
+  command => "sed -i \"/${user} soft/s/4/${limi}/\" /etc/security/limits.conf",
+  path    => $command_path,
 }
