0e4c131dfc6425daac3527900cb7f064c0078c35
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-05-16 19:34:02 +0300

    Task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,13 +2,3 @@ exec { 'Fix':
   command => 'sed -i "s/^ULIMIT=\"-n 15\"/ULIMIT=\"-n 8192\"/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
-
-service { 'nginx':
-  ensure    => 'stopped',
-  enable    => false
-}
-
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true
-}
